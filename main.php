2316d82f9aeec02885216ded842555502d191e94
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2024-11-25 22:39:33 -0800

    first commit

---
+++ main.php
@@ -0,0 +1,58 @@
+<?php
+echo "Hello World!";
+?>
+
+<div>
+<?php
+echo 4 + 2;
+echo 4 - 2;
+echo 4 * 2;
+echo 4 / 2;
+?>
+</div>
+
+
+<?php $tmp = 1; ?>
+<div><?php echo $tmp; ?></div>
+
+<?php
+$a = "PHP";
+$b = "is fun!";
+echo $a . " " . $b;
+?>
+
+<div>
+<?php
+$program = "PHP";
+echo $program . " is fun!";
+echo $program . " is easy!";
+?>
+</div>
+
+<?php
+var_dump(6);
+var_dump("6"); 
+?>
+
+<div>
+<?php
+var_dump(1 == 1);
+?>
+</div>
+
+<div>
+<?php
+$score = 40;
+if($score >= 60) {
+    echo "You passed!";
+} else {
+    echo "You failed!";
+}
+?>
+</div>
+
+<div>
+<?php
+
+?>
+</div>
